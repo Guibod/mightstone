ead4226c862dafe8b67bdc60e279376dfa3f22d6
Guillaume Boddaert <gboddaert@solocal.com>
2024-06-15 08:32:02 +0200

    refactor: dropped httpx_cache support in favor of hishel

--- examples/scryfall_async.py
+++ examples/scryfall_async.py
@@ -6,13 +6,13 @@ from mightstone.app import Mightstone
 async def run(m: Mightstone):
     print("boseiju matches:")
     found = 0
-    async for card in m.scryfall.search_async("boseiju"):
+    async for card in m.scryfall.search("boseiju"):
         found += 1
         print(f" - {card}")
 
     print(f"Found {found} instances of Boseiju")
 
-    print(list([x.name async for x in m.scryfall.search_async("thalia")]))
+    print(list([x.name async for x in m.scryfall.search("thalia")]))
 
 
 m = Mightstone()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,22 +1,18 @@
 import pathlib
 import unittest
 from pathlib import PosixPath
-from unittest.mock import patch
-
-import httpx_cache
-from mongomock_motor import AsyncMongoMockClient
+import hishel
 
 from mightstone import Mightstone
-from mightstone.config import MightstoneSettings
 
 
 class TestAppClass(unittest.TestCase):
     def test_singleton_on_two_distinct_app(self):
         m = Mightstone({"storage": {"implementation": "fake"}})
         self.assertEqual(m.config.http.cache.directory, None)
-        self.assertIsInstance(m.cache_transport, httpx_cache.AsyncCacheControlTransport)
-        self.assertIsInstance(m.cache_transport.cache.cache_dir, pathlib.Path)
-        self.assertIn("Mightstone/http", str(m.cache_transport.cache.cache_dir))
+        self.assertIsInstance(m.cache_transport, hishel.AsyncCacheTransport)
+        self.assertIsInstance(m.cache_transport._storage._base_path, pathlib.Path)
+        self.assertIn("Mightstone/http", str(m.cache_transport._storage._base_path))
 
         m2 = Mightstone(
             {
@@ -27,4 +23,4 @@ class TestAppClass(unittest.TestCase):
         self.assertIsInstance(m2.config.http.cache.directory, PosixPath)
         self.assertEqual(m2.config.http.cache.directory, PosixPath("/tmp/tata"))
 
-        self.assertEqual(m2.cache_transport.cache.cache_dir, PosixPath("/tmp/tata"))
+        self.assertEqual(m2.cache_transport._storage._base_path, PosixPath("/tmp/tata"))
